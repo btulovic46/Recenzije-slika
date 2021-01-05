afa089e8085b7b71b5467fad8cac9f0bc0b57a95
btulovic46 <borkotulovic9991@gmail.com>
2021-01-05 17:17:37 +0100

    Kreirana baza podataka, dodat sql kod

---
+++ PresentationLayer/dbo.Review.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Review] (
+    [Id]      INT            NOT NULL,
+    [Comment] NVARCHAR (250) NOT NULL,
+    [Grade]   REAL           NOT NULL,
+    [ReviewerId] INT NULL, 
+    [PictureId] INT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC), 
+    CONSTRAINT [FK_Review_Reviewer] FOREIGN KEY ([ReviewerId]) REFERENCES [Reviewer]([Id]), 
+    CONSTRAINT [FK_Review_Picture] FOREIGN KEY ([PictureId]) REFERENCES [Picture]([Id])
+);
---
+++ PresentationLayer/dbo.Reviewer.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Reviewer]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Email] NVARCHAR(50) NOT NULL, 
+    [Adress] NVARCHAR(50) NOT NULL, 
+    [City] NVARCHAR(50) NOT NULL, 
+    [PhoneNumber] DECIMAL(20) NOT NULL
+)
