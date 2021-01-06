81db85779bdb0f423ecb50627ff517c25822a886
btulovic46 <borkotulovic9991@gmail.com>
2021-01-06 13:31:04 +0100

    DL- Kreirana klasa Constants ( povezan DL sa bazom)

--- PresentationLayer/DataAccessLayer/Constants.cs
+++ PresentationLayer/DataAccessLayer/Constants.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BuisnessLayer
+namespace DataAccessLayer
 {
-    public class Class1
+    class Constants
     {
     }
 }
