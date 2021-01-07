023ad3f80940ae206e9a2781877aeb61f81eb586
Sava99 <savatrajkovic913@gmail.com>
2021-01-07 22:01:23 +0100

    Ubacene metode za unos jedne galerije i jednog reviewera, kao i metode za ispis svih reviewera i galeria

---
+++ PresentationLayer/BuisnessLayer/GalleryBuisness.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnessLayer
+{
+    public class GalleryBuisness
+    {
+        private readonly GalleryRepository galleryRepository;
+
+        public GalleryBuisness()
+        {
+            this.galleryRepository = new GalleryRepository();
+        }
+        public List<Gallery> GetAllGalleries()
+        {
+            return this.galleryRepository.GetAllGalleries();
+        }
+
+        public bool InsertGallery(Gallery g)
+        {
+            if (this.galleryRepository.InsertGallery(g) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ PresentationLayer/BuisnessLayer/ReviewerBuisness.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnessLayer
+{
+    public class ReviewerBuisness
+    {
+        private readonly ReviewerRepository reviewerRepository;
+
+        public ReviewerBuisness()
+        {
+            this.reviewerRepository = new ReviewerRepository();
+        }
+        public List<Reviewer> GetAllReviewers()
+        {
+            return this.reviewerRepository.GetAllReviewers();
+        }
+
+        public bool InsertReviewer(Reviewer r)
+        {
+            if (this.reviewerRepository.InsertReviewer(r) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
